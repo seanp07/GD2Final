69d0ccc759428aa6e5998adb0a3cd121c62cc33f
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-05-16 14:40:11 -0500

    update

--- test/test_world.gd
+++ test/test_world.gd
@@ -3,9 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player1 = $test_player2
-	var player2 = $test_rpg_player
-	var player = 0
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
