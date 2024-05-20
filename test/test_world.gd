13930c675625e3ef3f048e0b5dd5aa8ba7a2c2ab
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2024-05-20 15:26:17 -0500

    update 5/20

---
+++ test/test_world.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+var packed_scene = [
+	preload("res://test/minigun_player.tscn"),
+	preload("res://test/test_rpg_player.tscn"),
+	preload("res://player versions/player_rifle.tscn"),
+	preload("res://player versions/player_pistol.tscn"),
+	preload("res://player versions/player_revolver.tscn")
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var x = randi() % packed_scene.size()
+	var scene = packed_scene[x].instantiate()
+	add_child(scene)
+	scene.position = Vector3(0, 2, 0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
