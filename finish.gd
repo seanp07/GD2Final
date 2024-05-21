8149130f5c8d84bc61b6e803ecccdf1202f92939
seanp07 <palmer.s@stu.janesville.k12.wi.us>
2024-05-21 15:24:16 -0500

    5/21

--- finish.gd
+++ finish.gd
@@ -3,4 +3,4 @@ var held = false
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 			if held == true:
-				get_tree().quit()
+				get_tree().change_scene_to_file("res://worlds/wr_2.tscn")
--- player versions/player_rpg.gd
+++ player versions/player_rpg.gd
@@ -33,7 +33,7 @@ var spray_lock = 0.0
 var NORMAL_SPRAY_AMOUNT = 0.001
 var SPRAY_AMOUNT = NORMAL_SPRAY_AMOUNT
 var FIRING_DELAY = 0.001
-var ATTACK = 10
+var ATTACK = 100
 var NORMAL_HEIGHT = 2.0
 var NORMAL_COLLISION_RAD = 0.5
 var CROUCH_COLLISION_RAD = 0.8
@@ -41,7 +41,7 @@ var NORMAL_HEAD = 0.8
 
 var CLIP_SIZE = 1
 var AMMO = CLIP_SIZE
-var TOTAL_AMMO = 15
+var TOTAL_AMMO = 20
 var is_reloading = false
 
 @onready var audio_player = $AudioStreamPlayer3D
@@ -56,6 +56,8 @@ var aim_quat = euler_degrees_to_quat(Vector3(0, 0, 0))
 var target_pos = unaim_pos
 var target_quat = unaim_quat
 
+var timer = Timer
+var time = 16
 func degrees_to_radians(degrees: Vector3) -> Vector3:
 	return Vector3(
 		deg_to_rad(degrees.x),
@@ -79,7 +81,8 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	$HUD/time.text =  str(int(time))
+	time = max(time-delta, 0.0)
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY * 1.05
@@ -152,16 +155,7 @@ func _physics_process(delta):
 		OS.alert("You Died!")
 		get_tree().reload_current_scene()
 	
-	if len(get_tree().get_nodes_in_group("Enemy")) <= 0:
-		await get_tree().create_timer(0.25).timeout
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		OS.alert("You win!")
-		get_tree().quit()
-		
-		# if "fps_world2" in get_tree().current_scene.name:
-			# get_tree().change_scene_to_file("res://fps_world3.tscn")
-		# else:
-			# get_tree().change_scene_to_file("res://fps_world2.tscn")
+	
 	if int(HEALTH) <= 0:
 		HEALTH = 0
 		await get_tree().create_timer(0.25).timeout
